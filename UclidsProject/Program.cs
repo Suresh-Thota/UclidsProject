e5fe1e51a8ea9f81dd2c1fb78c35a7882aa05e55
Suresh-Thota <Suresh.Thota@innroad.com>
2020-08-10 21:35:05 +0530

    Add project files.

---
+++ UclidsProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UclidsProject
+{
+    class Program
+    {
+        public static int GCD(int a,int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            return GCD(b, a % b);
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter two numbers to find gcd :");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("GCD is: "+GCD(a,b));
+        }
+    }
+}
